2425ceb52afe744537da3e39de8d5b53c4a660fe
Veronika <ushakova.ucz@gmail.com>
2017-09-29 15:18:36 +0300

    add babel

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
+"use strict";
 
-const randByWeight = (a, b) => b.weight * Math.random() - a.weight * Math.random();
+var randByWeight = function randByWeight(a, b) {
+    return b.weight * Math.random() - a.weight * Math.random();
+};
 
 module.exports.avatarGenerate = function (male, layers, items) {
-    let out = [];
-    for (const layer of layers) {
-        const layerItems = [];
-        for (const item of items) {
-            if (item.avatarlayerId === layer.id && (item.sex === male || item.sex === 2)) {
-                layerItems.push(item);
+    var out = [];
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = layers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var layer = _step.value;
+
+            var layerItems = [];
+            var _iteratorNormalCompletion2 = true;
+            var _didIteratorError2 = false;
+            var _iteratorError2 = undefined;
+
+            try {
+                for (var _iterator2 = items[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                    var item = _step2.value;
+
+                    if (item.avatarlayerId === layer.id && (item.sex === male || item.sex === 2)) {
+                        layerItems.push(item);
+                    }
+                }
+            } catch (err) {
+                _didIteratorError2 = true;
+                _iteratorError2 = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                        _iterator2.return();
+                    }
+                } finally {
+                    if (_didIteratorError2) {
+                        throw _iteratorError2;
+                    }
+                }
+            }
+
+            out.push(layerItems.sort(randByWeight)[0].num);
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
             }
         }
-        out.push(layerItems.sort(randByWeight)[0].num);
     }
+
     return out;
-}
+};
